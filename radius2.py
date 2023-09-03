fcc8ba60850bc83fc4cafd021519438ccd07787b
kwongliik <46083661+kwongliik@users.noreply.github.com>
2023-09-03 12:50:31 +0800

    Update rectangle.py
    Calculate area of rectangle

--- radius2.py
+++ radius2.py
@@ -1,18 +1,24 @@
 # function to read input
-def read_input(): 
-    r = float(input("key in radius: "))
-    return r
+def get_width_input(): 
+    w = float(input("key in width: "))
+    return w
 
+def get_length_input(): 
+    l = float(input("key in length: "))
+    return l
+    
 # function to calculate the area of circle
-def calculate(r):
-    pi = 3.142
-    area = pi * r ** 2
+def calculate(w, l):
+    W = get_width_input()
+    L = get_length_input()
+    area = W * L
     return area
 
 # main
 def main():
-    r = read_input()
-    area = calculate(r)
+    x = get_width_input()
+    y = get_length_input()
+    area = calculate(x, y)
     print(f"Area: {area}")
 
 if __name__ == "__main__":
